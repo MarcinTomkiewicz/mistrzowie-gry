f4f0037d7a42f7a47a79e3516f69704881a77c0f
Marcin <marc.tomk@gmail.com>
2026-02-06 10:53:10 +0100

    Initial commit and SCSS settings

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,13 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'mistrzowie-gry';
+  setTheme(theme: 'dark' | 'light') {
+    const root = document.documentElement;
+    if (theme === 'light') root.setAttribute('data-theme', 'light');
+    else root.removeAttribute('data-theme'); // domyślnie dark na :root
+  }
 }
