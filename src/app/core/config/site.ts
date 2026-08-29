d6446ed68033c7b5a1536c009413610323a736e8
Marcin <marc.tomk@gmail.com>
2026-08-29 06:21:21 +0200

    feat: update venue name from 'Pub Pod Dwoma Miotlami' to 'Shept' in site configuration and meeting details

--- src/app/core/config/site.ts
+++ src/app/core/config/site.ts
@@ -12,7 +12,7 @@ export const THEME_DARK_BRAND_IMAGE = 'theme/dark/brand.avif';
 export const THEME_LIGHT_FOOTER_IMAGE = 'theme/light/footer.avif';
 export const THEME_DARK_FOOTER_IMAGE = 'theme/dark/footer.avif';
 
-export const VENUE_NAME = 'Pub Pod Dwoma Miotlami';
+export const VENUE_NAME = 'Shept';
 export const VENUE_STREET_ADDRESS = 'Wroniecka 21';
 export const VENUE_LOCALITY = 'Poznan';
 export const VENUE_COUNTRY = 'PL';
